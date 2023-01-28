35a4ae1030b2d7f65580e0b62beaac09a5d31fc0
Pranav P <pranavsdreams@gmail.com>
2023-01-29 02:31:03 +0530

    Updated installer_initials.sh

--- installer_initials.sh
+++ installer_initials.sh
@@ -1,26 +1,6 @@
 #!/bin/bash
 
 echo ""
-echo "Downloading Verilator"
-echo ""
-
-git clone https://github.com/verilator/verilator ~/.verilator  # Only first time
-
-# Every time you need to build:
-unset VERILATOR_ROOT  
-git --git-dir='~/.verilator/.git' pull         # Make sure git repository is up-to-date
-git --git-dir='~/.verilator/.git' checkout stable      # Use most recent stable release
-
-echo ""
-echo "Installing Verilator"
-echo ""
-
-$(cd ~/.verilator; autoconf)         # Create ./configure script
-./configure      # Configure and create Makefile
-make -j `nproc`  # Build Verilator itself (if error, try just 'make')
-sudo make install
-
-echo ""
 echo "Verilog GUI Installer"
 echo ""
 
@@ -33,6 +13,10 @@ ARCHIVE=$(awk '/^__ARCHIVE__/ {print NR + 1; exit 0; }' "${0}")
 tail -n+${ARCHIVE} "${0}" | tar xpJv -C ${DESTINATION}
 
 # Put your logic here (if you need)
+
+chmod +x ${HOME}/verilator_gui/dist/bin/*
+export VERILATOR_ROOT=${HOME}/verilator_gui/dist
+export PATH=$VERILATOR_ROOT/bin:$PATH
 
 echo ""
 echo "Installation complete."
